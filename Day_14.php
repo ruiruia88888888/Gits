426ac4162e0032a67d0387982531259a6f540332
朕锕 <ruiria88888888@163.com>
2019-04-26 08:55:54 +0800

    Day14

---
+++ Day_14.php
@@ -0,0 +1,30 @@
+<?php
+
+print_r(sum([1,2,3,4,5,6,7,8,9],10));
+
+function sum($arr,$num)
+{
+    $a=[];
+    $len=strlen($arr);
+
+    if($len<2)
+    {
+        return false;
+    }
+    for($i=0;$i<$len;$i++)
+    {
+        for($j=0;$j<$len;$j++)
+        {
+            if($arr[$i]+$arr[$j]==$num)
+            {
+                $a[]=$arr[$i]*$arr[$j];
+            }
+        }
+    }
+    sort($a);
+    return $a[0];
+}
+
+
+
+?>
